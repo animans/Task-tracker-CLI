f30acc4d32d4f599e0e5949fd2c4559bee999034
Anuar Baishagirov <baishagirov_a@mail.ru>
2025-07-22 05:34:56 +0600

    add method New

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
+	//task := task.Task{id:18, "age", true, "13.03.2025, 23:25", "14.03.2025, 20:25"}
 	var task task.Task
-	fmt.Println(task)
+	task.New(18, "age", true, "13.03.2025, 23:25", "14.03.2025, 20:25")
+	task.TaskPrint()
+	fmt.Println()
 }
--- task/task.go
+++ task/task.go
@@ -1,9 +1,24 @@
 package task
 
+import "fmt"
+
 type Task struct {
 	id          int
 	description string
 	status      bool
 	createdAt   string
 	uploatedAt  string
+}
+
+func (t *Task) New(a int, d string, s bool, c string, u string) {
+	task := Task{id: a, description: d, status: s, createdAt: c, uploatedAt: u}
+	*t = task
+}
+
+func (t Task) TaskPrint() {
+	fmt.Printf("id: %d\n", t.id)
+	fmt.Printf("description: %s\n", t.description)
+	fmt.Printf("status: %t\n", t.status)
+	fmt.Printf("createdAt: %s\n", t.createdAt)
+	fmt.Printf("uploatedAt: %s\n", t.uploatedAt)
 }
