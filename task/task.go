7dd615060c3c615900b0369ba2ac5027318927f8
Anuar Baishagirov <baishagirov_a@mail.ru>
2025-07-22 22:35:04 +0600

    add add, settrack, findtasktracker, print methods

--- task/task.go
+++ task/task.go
@@ -1,6 +1,10 @@
 package task
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
 
 type Task struct {
 	Id          int    `json:"id"`
@@ -20,4 +24,71 @@ func (t Task) TaskPrint() {
 	fmt.Printf("status: %t\n", t.Status)
 	fmt.Printf("createdAt: %s\n", t.CreatedAt)
 	fmt.Printf("uploatedAt: %s\n", t.UploatedAt)
+}
+
+type TaskTracker struct {
+	Size      int
+	TaskTrack []Task
+}
+
+func (t *TaskTracker) Add(str string) {
+	if t.FindTaskInTracker(str) {
+		fmt.Printf("Error, this Task alrady exist\n")
+		return
+	}
+	task := t.SetTrack(str)
+	t.Size++
+	t.TaskTrack = append(t.TaskTrack, task)
+}
+
+func (t *TaskTracker) SetTrack(str string) Task {
+	id := t.Size + 1
+	status := false
+	year := strconv.Itoa(time.Now().Year())
+	mo := int(time.Now().Month())
+	month := ""
+	if mo < 10 {
+		month = "0" + strconv.Itoa(mo)
+	} else {
+		month = strconv.Itoa(mo)
+	}
+	d := time.Now().Day()
+	day := ""
+	if d < 10 {
+		day = "0" + strconv.Itoa(d)
+	} else {
+		day = strconv.Itoa(d)
+	}
+	h := time.Now().Hour()
+	hours := ""
+	if h < 10 {
+		hours = "0" + strconv.Itoa(h)
+	} else {
+		hours = strconv.Itoa(h)
+	}
+	m := time.Now().Minute()
+	minute := ""
+	if m < 10 {
+		minute = "0" + strconv.Itoa(m)
+	} else {
+		minute = strconv.Itoa(m)
+	}
+	createdAt := day + "." + month + "." + year + ", " + hours + ":" + minute
+	uploatedAt := ""
+	return Task{id, str, status, createdAt, uploatedAt}
+}
+
+func (t *TaskTracker) FindTaskInTracker(str string) bool {
+	for i := 0; i < t.Size; i++ {
+		if t.TaskTrack[i].Description == str {
+			return true
+		}
+	}
+	return false
+}
+
+func (t TaskTracker) Print() {
+	for i := 0; i < t.Size; i++ {
+		t.TaskTrack[i].TaskPrint()
+	}
 }
