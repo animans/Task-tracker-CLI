908b7ed0d134e6b3296a0927b1cb17463240b692
Anuar Baishagirov <baishagirov_a@mail.ru>
2025-07-22 17:55:34 +0600

    add json tag, change struct type, upload method

--- task/task.go
+++ task/task.go
@@ -3,22 +3,21 @@ package task
 import "fmt"
 
 type Task struct {
-	id          int
-	description string
-	status      bool
-	createdAt   string
-	uploatedAt  string
+	Id          int    `json:"id"`
+	Description string `json:"description"`
+	Status      bool   `json:"status"`
+	CreatedAt   string `json:"createdAt"`
+	UploatedAt  string `json:"uploatedAt"`
 }
 
 func (t *Task) New(a int, d string, s bool, c string, u string) {
-	task := Task{id: a, description: d, status: s, createdAt: c, uploatedAt: u}
-	*t = task
+	*t = Task{Id: a, Description: d, Status: s, CreatedAt: c, UploatedAt: u}
 }
 
 func (t Task) TaskPrint() {
-	fmt.Printf("id: %d\n", t.id)
-	fmt.Printf("description: %s\n", t.description)
-	fmt.Printf("status: %t\n", t.status)
-	fmt.Printf("createdAt: %s\n", t.createdAt)
-	fmt.Printf("uploatedAt: %s\n", t.uploatedAt)
+	fmt.Printf("id: %d\n", t.Id)
+	fmt.Printf("description: %s\n", t.Description)
+	fmt.Printf("status: %t\n", t.Status)
+	fmt.Printf("createdAt: %s\n", t.CreatedAt)
+	fmt.Printf("uploatedAt: %s\n", t.UploatedAt)
 }
